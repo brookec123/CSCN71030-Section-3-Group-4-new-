2a5606edf6af6b2c3244002dc291e9e091964bd7
stephanp123 <145160503+stephanp123@users.noreply.github.com>
2024-04-05 15:10:30 -0400

    Updated main to include arguments

--- Yhatzee/Game/Source.c
+++ Yhatzee/Game/Source.c
@@ -3,9 +3,16 @@
 /// Course Code: CSCN71030 Group 4
 /// Description: 
 #include "GameLogic.h"
+#include "UI.h"
 
-int main()
-{
-   
+int main(int argc, char* argv) {
+
+    if (argv[1] == 1) {
+
+        
+
+    }
+
+
     return 0;
 }
