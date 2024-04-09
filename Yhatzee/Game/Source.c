7f3ac4254343defc7183a6fcfdf1443ce96b9771
stephanp123 <145160503+stephanp123@users.noreply.github.com>
2024-04-08 23:44:21 -0400

    Update Source.c - main collects player data & game

--- Yhatzee/Game/Source.c
+++ Yhatzee/Game/Source.c
@@ -3,11 +3,17 @@
 /// Course Code: CSCN71030 Group 4
 /// Description: 
 #include "GameLogic.h"
+#include "Die.h"
+#include "Player.h"
 #include "UI.h"
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
 
     if (argc != 2) {
+
+        printf("%d", argc);
+
+        printf("%s", argv[1]);
 
         fprintf(stderr, "\n\nERROR: No players.\n\n");
 
@@ -18,10 +24,23 @@ int main(int argc, char* argv) {
     UI u;
 
     u.numOfPlayers = atoi(argv[1]);
+    
+    char name[100];
 
+
+    for (int i = 0; i < u.numOfPlayers; i++) {
+
+       printf("Enter player name: ");
+
+       fgets(name, 100, stdin);
+
+       u.p[i] = CreateNewPlayer(name);
+
+    }
+    
     printMainMenu(u);
-
+    
     menuChoice(u);
-
+ 
     return 0;
 }
